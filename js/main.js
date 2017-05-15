3d59a27f26530a891ee05c4b21a59925c9cc4698
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-05-15 15:01:24 -0400

    añadiendo archivos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+var nombre = document.getElementById('name').value;
+var apellido = document.getElementById('lastname').value;
+var correo = document.getElementById('input-email').value;
+var contrasenia = document.getElementById('input-password').value;
+var twitter = document.getElementById('input-social').value;
+var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+
+//hacer las validaciones
+if(nombre.toUpperCase() === "" || apellido.toUpperCase() === "" || correo === "" || contrasenia === "" ||twitter===""){
+	alert("Todos los campos son obligatorios");
+	return false;
 }
+else if 
+	(!regex.test(correo)){
+	alert('El correo no es valido');
+	return false;
+}
+else if
+	(contrasenia != "password" && contrasenia.length >= 6){
+		alert ("ingrese una contrasenia valida");
+	return false;
+}
+
+
+
+}	
+
+
+		
+
+
+
