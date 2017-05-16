39e54e6f6f8159f35d51aeee5a8aedada74ed282
ValentinaRod <v.rodriguez1425@gmail.com>
2017-05-15 23:44:05 -0400

    añadiendo

--- js/main.js
+++ js/main.js
@@ -1,33 +1,62 @@
 function validateForm(){
-var nombre = document.getElementById('name').value;
-var apellido = document.getElementById('lastname').value;
-var correo = document.getElementById('input-email').value;
-var contrasenia = document.getElementById('input-password').value;
-var twitter = document.getElementById('input-social').value;
-var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+
 
 //hacer las validaciones
-if(nombre.toUpperCase() === "" || apellido.toUpperCase() === "" || correo === "" || contrasenia === "" ||twitter===""){
-	alert("Todos los campos son obligatorios");
-	return false;
+function nombre(){
+    var nombre = document.getElementById("name").value;
+    if(!(/^[A-Z][a-z]{3,19}$/).test(nombre)) {
+
+    }
+    var cajaDiv = document.getElementsByClassName("name-container input-box")[0];
+    var x = document.createElement("span");
+    var t = document.createTextNode("Escribe tu nombre correcto");
+    x.appendChild(t);
+    cajaDiv.appendChild(x);
 }
-else if 
-	(!regex.test(correo)){
-	alert('El correo no es valido');
-	return false;
+nombre();
+
+function apellido(){
+var apellido = document.getElementById('lastname').value;
+if(!(/^[A-Z][a-z]{3,19}$/).test(apellido)) {
+
+    }
+    var cajaDiv = document.getElementsByClassName("lastname-container input-box")[0];
+    var x = document.createElement("span");
+    var t = document.createTextNode("Escribe tu apellido correcto");
+    x.appendChild(t);
+    cajaDiv.appendChild(x);
 }
-else if
-	(contrasenia != "password" && contrasenia.length >= 6){
-		alert ("ingrese una contrasenia valida");
-	return false;
+apellido();
+
+function correo(){
+ if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
+
 }
+ var correo = document.getElementById('input-email').value;
+ if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
+ 
+}
+	var cajaDiv = document.getElementsByClassName("email-container input-box")[0];
+    var x = document.createElement("span");
+    var t = document.createTextNode("Escribe un Correo Electronico valido");
+    x.appendChild(t);
+    cajaDiv.appendChild(x);
 
+}
+correo();
 
+function contrasenia (){
+	var patronPass= /^[a-zA-Z0-9]*$/;
+	var contrasenia = document.getElementById('input-password').value;
+		if (patronPass.test(contrasenia) !== "password" || contrasenia.length >= 6){
 
-}	
+}
+	var cajaDiv = document.getElementsByClassName("form-group input-box")[0];
+    var x = document.createElement("span");
+    var t = document.createTextNode("Escribe una contraseña valida");
+    x.appendChild(t);
+    cajaDiv.appendChild(x);
+}
+contrasenia();
 
-
-		
-
-
-
+}
